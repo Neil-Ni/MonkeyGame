01eeb142b66c57e3b915a7fd81ce4140b4e74ccb
Neil Ni <tni@wesleyan.edu>
2012-03-02 09:38:07 +0800

    Initial Commit

---
+++ MonkeyGame/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MonkeyGame
+//
+//  Created by Tzu-Yang Ni on 3/2/12.
+//  Copyright (c) 2012 yoseka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MonkeyGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MonkeyGame
+//
+//  Created by Tzu-Yang Ni on 3/2/12.
+//  Copyright (c) 2012 yoseka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
